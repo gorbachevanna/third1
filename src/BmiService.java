5855d4c82e562bed5fd86fe8d6aa7d9a1843d2f3
Anna Gorbacheva <anja_k91@list.ru>
2024-04-27 17:46:40 +0300

    commit return

--- src/BmiService.java
+++ src/BmiService.java
@@ -3,6 +3,6 @@ public class BmiService {
     public double calculate(double weightKg, double heightM) {
         double index;
         index = weightKg / heightM / heightM;
-        return index;
+        return (int) index;
     }
 }
